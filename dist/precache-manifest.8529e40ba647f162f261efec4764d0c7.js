f7b68e0549b71a5f260221d8421a576c15b68ad7
naguilar12 <n.aguilar@uniandes.edu.co>
2020-09-22 14:20:33 -0500

    Carpeta dist

---
+++ dist/precache-manifest.8529e40ba647f162f261efec4764d0c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf5a25e8daac3558d1981a425945ebb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b353266b3dcbb2219419",
+    "url": "/static/css/2.e7b8816b.chunk.css"
+  },
+  {
+    "revision": "59d8d4f40493885db586",
+    "url": "/static/css/main.29172b1e.chunk.css"
+  },
+  {
+    "revision": "b353266b3dcbb2219419",
+    "url": "/static/js/2.4684657c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4684657c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59d8d4f40493885db586",
+    "url": "/static/js/main.9c5812df.chunk.js"
+  },
+  {
+    "revision": "f54ee2d0f258b54963df",
+    "url": "/static/js/runtime-main.b157fd5a.js"
+  },
+  {
+    "revision": "f4360658c60d515d990ca6be2d27010a",
+    "url": "/static/media/back-2-.f4360658.jpg"
+  }
+]);
